34d860f0ea24556345e0a9f08d1084dba847140d
LJeanmougin <louison.jeanmougin@irit.fr>
2024-10-29 16:32:59 +0100

    Added positional argument to get_stats.sh to select the bench from rodinia_2.0-ft

--- get_stats.sh
+++ get_stats.sh
@@ -1,6 +1,6 @@
-readarray KernelNames < <(grep kernel_name ./sim_run_11.0/backprop*/4096*/TEST_CFG/*.o*)
-readarray GpuSimCycles < <(grep gpu_sim_cycle ./sim_run_11.0/backprop*/4096*/TEST_CFG/*.o*)
-readarray GpuSimInsn < <(grep gpu_sim_insn ./sim_run_11.0/backprop*/4096*/TEST_CFG/*.o*)
+readarray KernelNames < <(grep kernel_name ./sim_run_11.0/$1*/*/TEST_CFG/*.o*)
+readarray GpuSimCycles < <(grep gpu_sim_cycle ./sim_run_11.0/$1*/*/TEST_CFG/*.o*)
+readarray GpuSimInsn < <(grep gpu_sim_insn ./sim_run_11.0/$1*/*/TEST_CFG/*.o*)
 index=0
 echo "################### Bench Results ###################"
 for name in "${KernelNames[@]}"

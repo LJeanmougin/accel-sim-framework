fcb924d8d71113ebc115317714f2c14c2e805d88
LJeanmougin <louison.jeanmougin@irit.fr>
2024-10-30 16:55:35 +0100

    Explicitly specified to run local jobs through cleanrun.sh

--- cleanrun.sh
+++ cleanrun.sh
@@ -4,4 +4,7 @@ export PATH=$CUDA_INSTALL_PATH/bin:$PATH
 source ./gpu-app-collection/src/setup_environment
 source ./gpu-simulator/setup_environment.sh
 make -j -C ./gpu-simulator/
-./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C TEST_CFG -N test
+./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C TEST_CFG -N test -l local
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "${RED}If jobs don't launch properly, clean the content of ./util/job_launching/procman and ./util/job_launching/logfiles${NC}"

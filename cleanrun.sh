3cdf4270ba944af6ec74f45e37cb02d6880a82f0
LJeanmougin <louison.jeanmougin@irit.fr>
2024-12-11 11:16:02 +0100

    Keeping everything up to date

--- cleanrun.sh
+++ cleanrun.sh
@@ -5,7 +5,7 @@ source ./gpu-app-collection/src/setup_environment
 source ./gpu-simulator/setup_environment.sh
 make -j -C ./gpu-app-collection/src rodinia_2.0-ft
 make -j -C ./gpu-simulator/
-./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C TEST_CFG -N test -l local
+./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C ORIN-PTX -N test -l local
 RED='\033[0;31m'
 NC='\033[0m'
 echo -e "${RED}If jobs don't launch properly, clean the content of ./util/job_launching/procman and ./util/job_launching/logfiles${NC}"

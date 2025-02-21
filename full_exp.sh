7bc5914d4af2f9fc3579017661e8e098bdcc269e
LJeanmougin <louison.jeanmougin@irit.fr>
2025-02-21 10:53:39 +0100

    Adding SGEMM and fullexp

--- full_exp.sh
+++ full_exp.sh
@@ -4,14 +4,13 @@ source ./gpu-app-collection/src/setup_environment
 source ./gpu-simulator/setup_environment.sh
 make -j -C ./gpu-app-collection/src rodinia-3.1
 make -j -C ./gpu-simulator/
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_5l-PTX -N r30_1b_5l -l local -r sim_r30_1b_5l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_10l-PTX -N r30_1b_10l -l local -r sim_r30_1b_10l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_25l-PTX -N r30_1b_25l -l local -r sim_r30_1b_25l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_50l-PTX -N r30_1b_50l -l local -r sim_r30_1b_50l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_100l-PTX -N test -l local -r sim_r30_1b_100l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_200l-PTX -N test -l local -r sim_r30_1b_200l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_400l-PTX -N test -l local -r sim_r30_1b_400l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_800l-PTX -N test -l local -r sim_r30_1b_800l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_5l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_5l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_10l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_10l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_25l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_25l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_50l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_50l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_100l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_100l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_200l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_200l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_400l-PTX -N rodinia -l local -r sim_rodinia-3.1_1b_400l
 RED='\033[0;31m'
 NC='\033[0m'
 echo -e "${RED}If jobs don't launch properly, clean the content of ./util/job_launching/procman and ./util/job_launching/logfiles${NC}"

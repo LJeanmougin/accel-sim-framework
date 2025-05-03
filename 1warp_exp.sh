380b912a41347dbc86c24209a92ab4c351d34719
LJeanmougin <louison.jeanmougin@irit.fr>
2025-05-03 11:21:53 +0200

    Added 1 warp script and bench for rodinia

--- 1warp_exp.sh
+++ 1warp_exp.sh
@@ -1,5 +1,6 @@
 ./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_5l_lrr-PTX -N rodinia-3.1_1w -l local -r ecrts_1w_5l
 ./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_10l_lrr-PTX -N rodinia-3.1_1w -l local -r ecrts_1w_10l
+./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_10l_lrr-PTX -N rodinia-3.1_1w -l local -r ecrts_1w_25l
 ./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_50l_lrr-PTX -N rodinia-3.1_1w -l local -r ecrts_1w_50l
 ./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_100l_lrr-PTX -N rodinia-3.1_1w -l local -r ecrts_1w_100l
 ./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_200l_lrr-PTX -N rodinia-3.1_1w -l local -r ecrts_1w_200l

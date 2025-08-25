aeb2769ecc680502bc3733bcc87319accd0fb370
LJeanmougin <louison.jeanmougin@irit.fr>
2025-08-25 17:23:26 +0200

    Setting up so rodinia and rodinia_1w can be built at the same time

--- full_exp.sh
+++ full_exp.sh
@@ -2,11 +2,11 @@ export CUDA_INSTALL_PATH=/usr/local/cuda-11.0/
 export PATH=$CUDA_INSTALL_PATH/bin:$PATH
 source ./gpu-app-collection/src/setup_environment
 source ./gpu-simulator/setup_environment.sh
-make -j -C ./gpu-app-collection/src rodinia-3.1
+make -j -C ./gpu-app-collection/src rodinia-3.1_1w
 make -j -C ./gpu-simulator/
 # ./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_5l-PTX -N rodinia-3.1 -l local -r ecrts_1b_5l
 # ./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_10l-PTX -N rodinia-3.1 -l local -r ecrts_1b_10l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_5l-PTX -N rodinia-3.1 -l local -r rodinia_1b_5l_lrr
+./util/job_launching/run_simulations.py -B rodinia-3.1_1w -C ORIN_5l-PTX -N rodinia-3.1_1w -l local -r rodinia_1w_5l
 # ./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_50l-PTX -N rodinia-3.1 -l local -r ecrts_1b_50l
 # ./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_100l-PTX -N rodinia-3.1 -l local -r ecrts_1b_100l
 # ./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_200l-PTX -N rodinia-3.1 -l local -r ecrts_1b_200l

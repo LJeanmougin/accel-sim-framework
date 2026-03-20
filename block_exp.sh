f7361ccf5c697cb80c3594d6a01760a2775afd85
LJeanmougin <louison.jeanmougin@irit.fr>
2026-03-20 10:25:18 +0100

    Added scripts for warp exp and configs for different schedulers

--- block_exp.sh
+++ block_exp.sh
@@ -4,13 +4,13 @@ source ./gpu-app-collection/src/setup_environment
 source ./gpu-simulator/setup_environment.sh
 make -j -C ./gpu-app-collection/src rodinia-3.1
 make -j -C ./gpu-simulator/
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_5l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_5l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_10l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_10l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_25l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_25l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_50l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_50l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_100l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_100l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_200l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_200l
-./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_400l-PTX -N rodinia-3.1 -l local -r sim_full/rodinia_1b_400l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_5l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_5l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_10l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_10l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_25l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_25l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_50l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_50l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_100l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_100l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_200l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_200l
+./util/job_launching/run_simulations.py -B rodinia-3.1 -C ORIN_400l_gto-PTX -N rodinia-3.1 -l local -r sim_full_gto/rodinia_1b_400l
 RED='\033[0;31m'
 NC='\033[0m'
 echo -e "${RED}If jobs don't launch properly, clean the content of ./util/job_launching/procman and ./util/job_launching/logfiles${NC}"
